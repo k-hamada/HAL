c908ad78569b34abd7de3390675da67ca8c09efb
Ryota Kanai <rkanai@aiming-inc.com>
2017-06-22 12:55:05 +0900

    [client] アイテム取得時のイベントを用意

--- Client/Assets/Scripts/ItemController.cs
+++ Client/Assets/Scripts/ItemController.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
     public int ItemId;
+    public event Action OnGot;
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.LogError("OnTriggerEnter");
+        OnGot();
     }
 }
